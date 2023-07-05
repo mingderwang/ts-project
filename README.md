4d50ab0335550b4cd28ee4a1e891cf53268e4481
Ming-der Wang <mingderwang@me.com>
2023-07-05 23:51:20 +0800

    add config for jest include paths and exclude paths

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * pnpm
 * jest
 * eslint
+* mitosis
--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+   // Specify the path(s) you want to include for testing
+   roots: ['<rootDir>/src/__tests__'],
+
+   // Specify the path(s) you want to exclude from testing
+   testPathIgnorePatterns: ['<rootDir>/output'],
 };
