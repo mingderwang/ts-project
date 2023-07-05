b950532897deb6c2e2bc4f0202810670fe469200
Ming-der Wang <mingderwang@me.com>
2023-07-05 15:59:55 +0800

    add mitosis and eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -5,14 +5,20 @@ module.exports = {
       ecmaVersion: 2021,
       sourceType: 'module',
       project: './tsconfig.json',
+      ecmaFeatures: {
+        jsx: true,
+      },
     },
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', '@builder.io/mitosis'],
     extends: [
       'eslint:recommended',
       'plugin:@typescript-eslint/recommended',
       'prettier',
+      // Use this approach for our recommended rules configuration
+      'plugin:@builder.io/mitosis/recommended',
     ],
     rules: {
-      // Customize your rules here
+      // Use this to configure rules individually
+      '@builder.io/mitosis/css-no-vars': 'error',
     },
   };
